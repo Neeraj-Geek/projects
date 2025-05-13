8698e41ffae8b7e374bf1d34a14a8331d4b084bd
Neeraj-Geek <neerajk0128@gmail.com>
2025-05-13 22:46:39 +0530

    delete old

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import { app } from "./app.js";
-dotenv.config({ path: "../.env" });
-
-let Port = process.env.PORT || 8080;
-app.listen(Port, () => {
-  console.log(`⚙️ Server is running at port : ${Port}`);
-});
