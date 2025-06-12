f29841abf3b8966e666e95b040cee36bb211fa58
Neeraj-Geek <neerajk0128@gmail.com>
2025-06-12 21:31:32 +0530

    added public profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,11 +2,49 @@ import { User } from "../model/userModel.js";
 import { responseUtil } from "../utils/resposneUtil.js";
 
 export const userProfileController = async (req, res) => {
-  const user = await User.findOne(
-    { _id: req.user.userId },
-    { _id: 0, email: 1, name: 1 }
-  );
-  console.log(user);
-  //   return responseUtil(user, "User data", 200, true, {});
-  return responseUtil(res, "User data", 200, true, { user });
+  try {
+    const user = await User.findOne(
+      { _id: req.user.userId },
+      { _id: 0, email: 1, name: 1 }
+    );
+    console.log(user);
+    //   return responseUtil(user, "User data", 200, true, {});
+    return responseUtil(res, "User data", 200, true, { user });
+  } catch (error) {
+    return responseUtil(res, "No user found with that username.", 404, false, {
+      error: "No user found with that username.",
+    });
+  }
+};
+
+export const userUpdateProfileController = async (req, res) => {
+  try {
+    const updatedName = req.body.name;
+    const user = await User.updateOne(
+      { _id: req.user.userId },
+      { $set: { name: updatedName } }
+    );
+    console.log(user);
+    return responseUtil(res, "Profile Updated", 200, true, { user });
+  } catch (error) {
+    return responseUtil(res, "No user found with that username.", 404, false, {
+      error: "No user found with that username.",
+    });
+  }
+};
+
+export const userPublicProfileController = async (req, res) => {
+  try {
+    const user = await User.findOne(
+      { _id: req.params.id },
+      { _id: 0, email: 1, name: 1 }
+    );
+    console.log(user);
+    //   return responseUtil(user, "User data", 200, true, {});
+    return responseUtil(res, "User data", 200, true, { user });
+  } catch (error) {
+    return responseUtil(res, "No user found with that username.", 404, false, {
+      error: "No user found with that username.",
+    });
+  }
 };
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { userProfileController } from "../controllers/userController.js";
+import {
+  userProfileController,
+  userPublicProfileController,
+  userUpdateProfileController,
+} from "../controllers/userController.js";
 import { tokenCheck } from "../utils/verifyJwtToken.js";
 
 const userRoute = Router();
+// self profile
 userRoute.get("/me", tokenCheck, userProfileController);
+userRoute.put("/me", tokenCheck, userUpdateProfileController);
+
+// public Profile routes
+userRoute.get("/:id", tokenCheck, userPublicProfileController);
 
 export default userRoute;
